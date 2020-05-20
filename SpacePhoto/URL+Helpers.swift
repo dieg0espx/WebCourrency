a18a1fd5ec5c0510be2c3bf47919f8af6f2505f0
dael <espinosa9@me.com>
2020-05-19 22:55:12 -0700

    first commit

---
+++ SpacePhoto/URL+Helpers.swift
@@ -0,0 +1,25 @@
+
+//
+//  URL+Helpers.swift
+//  SpacePhoto
+//
+//  Created by Diego Espinosa on 2020-05-19.
+//  Copyright © 2020 Diego Espinosa. All rights reserved.
+//
+
+import Foundation
+
+extension URL {
+    func withQueries(_ queries: [String: String]) -> URL? {
+        var components = URLComponents(url: self, resolvingAgainstBaseURL: true)
+        components?.queryItems = queries.map
+        { URLQueryItem(name: $0.0, value: $0.1) }
+        return components?.url
+    }
+    
+    func withHTTPS() -> URL? {
+        var components = URLComponents(url: self, resolvingAgainstBaseURL: true)
+        components?.scheme = "https"
+        return components?.url
+    }
+}
--- SpacePhoto/ViewController.swift
+++ SpacePhoto/ViewController.swift
@@ -9,12 +9,56 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var descriptionLabel: UILabel!
+    @IBOutlet weak var copyrightLabel: UILabel!
+    @IBOutlet weak var imageView: UIImageView!
+    
+    let photoInfoController = PhotoInfoController()
+    
     override func viewDidLoad() {
+        
+       // UIApplication.shared.isNetworkActivityIndicatorVisible = true
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        descriptionLabel.text = ""
+        copyrightLabel.text = ""
+        
+//        photoInfoController.fetchPhotoInfo { (photoInfo) in
+//            guard let photoInfo = photoInfo else { return }
+//            DispatchQueue.main.async {
+//                self.title = photoInfo.title
+//                self.descriptionLabel.text = photoInfo.description
+//                if let copyright = photoInfo.copyright {
+//                    self.copyrightLabel.text = "Copyright \(copyright)"
+//                } else {
+//                    self.copyrightLabel.isHidden = true
+//                }
+//            }
+//        }
+        
+        photoInfoController.fetchPhotoInfo { (photoInfo) in
+            if let photoInfo = photoInfo {
+                self.updateUI(with: photoInfo)
+            }
+        }
     }
-
-
+    
+    func updateUI(with photoInfo: PhotoInfo) {
+        guard let url = photoInfo.url.withHTTPS() else { return }
+        let task = URLSession.shared.dataTask(with: url,
+            completionHandler: { (data, response, error) in guard let data = data,
+            let image = UIImage(data: data) else { return }
+            DispatchQueue.main.async {
+                self.title = photoInfo.title
+                self.imageView.image = image
+                self.descriptionLabel.text = photoInfo.description
+                if let copyright = photoInfo.copyright {
+                    self.copyrightLabel.text = "Copyright \(copyright)"
+                } else {
+                    self.copyrightLabel.isHidden = true
+                }
+            }
+        })
+        task.resume()
+    }
 }
 
