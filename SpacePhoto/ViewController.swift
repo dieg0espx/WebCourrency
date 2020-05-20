38e7790931ad6a20dc94d00470ab5f6cb93d274e
dael <espinosa9@me.com>
2020-05-19 21:05:08 -0700

    Initial Commit

---
+++ SpacePhoto/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SpacePhoto
+//
+//  Created by Diego Espinosa on 2020-05-19.
+//  Copyright © 2020 Diego Espinosa. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
